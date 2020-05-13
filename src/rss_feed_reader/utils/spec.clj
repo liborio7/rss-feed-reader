079304b82c57c3dde3ae1e2bb6023ac5faca5f27
liborio7 <giuseppe.cafarelli.88@gmail.com>
2020-05-13 11:11:58 +0200

    enhance spec errors description

--- src/rss_feed_reader/utils/spec.clj
+++ src/rss_feed_reader/utils/spec.clj
@@ -1,7 +1,17 @@
 (ns rss-feed-reader.utils.spec
   (:require [clojure.spec.alpha :as s]))
 
+(defn describe-problem [problem]
+  (let [path (:path problem)
+        pred (:pred problem)
+        pred (if (coll? pred)
+               ((comp (juxt first last) last) pred)
+               pred)]
+    (->> [path pred]
+         (flatten)
+         (map name))))
+
 (defn errors [spec data]
   (->> (s/explain-data spec data)
-       :clojure.spec.alpha/problems
-       (map (fn [m] [(:path m) (:pred m)]))))
+       (:clojure.spec.alpha/problems)
+       (map describe-problem)))
