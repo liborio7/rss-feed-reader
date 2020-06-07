cfea4e8723694bd240022723c98060d099c9043f
liborio7 <giuseppe.cafarelli.88@gmail.com>
2020-06-08 00:11:28 +0200

    add dev repl file

---
+++ dev/repl.clj
@@ -0,0 +1,14 @@
+(ns repl)
+
+
+(defn transducer-debug []
+  (fn [rf]
+    (let [step (atom 0)]
+      (fn
+        ([] (rf))
+        ([result] (rf result))
+        ([result input]
+         (prn ">>>" @step)
+         (prn "input:" input)
+         (prn "<<<")
+         (rf result input))))))
